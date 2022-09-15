958ae64713eb42b77eef1f385ae7abee53335f05
Yucca99 <113177808+Yucca99@users.noreply.github.com>
2022-09-15 13:44:53 +0200

    implemented simple mutate function

--- specialist_solution_v2.py
+++ specialist_solution_v2.py
@@ -1,12 +1,14 @@
 import numpy as np
+import random
 from numpy.random import default_rng
 
+mut_rate = 0.2
 
-def init_population(num_offspring, _n_hidden):
+def init_population(pop_size, _n_hidden):
     # each offspring has a list of weights with size sum_i(size(l_i-1) * size(l_i))
     seed = 42
-    num_inputs = 8
-    num_output = 20
+    num_inputs = 20
+    num_output = 5
     init_bias = 0.0
 
     # num neurons
@@ -16,11 +18,30 @@ def init_population(num_offspring, _n_hidden):
 
     # TODO do this more efficient!
     pop = []
-    for i in range(num_offspring):
+    for i in range(pop_size):
         pop.append(default_rng(seed).random(sum) + init_bias)
         seed += 1
 
     return np.array(pop)
 
+def mutate(pop):
+    pop_offspring = []
+    lower = np.min(pop)
+    upper = np.max(pop)
+    for genome in pop:
+        offspring = []
+        for gene in genome:
+            mutate = np.random.uniform(0, 1)
+            if mutate <= mut_rate:
+                offspring.append(random.uniform(lower, upper))
+            else:
+                offspring.append(gene)
+        pop_offspring.append(offspring)
+    return np.array(pop_offspring)
 
-print(init_population(2, np.array([2, 2])))
+    # draw probability from distribution, if prob <= mutation threshold, mutate gene
+    # mutate gene by changing it into random value between lower and upper domain
+test_pop = init_population(2, np.array([2, 2]))
+print(test_pop)
+print(len(test_pop[0]))
+print(mutate(test_pop))
