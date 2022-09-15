536a7d78131bc2b932cedc2b7967772e9b5ab748
paddy <paddy-hofmann@web.de>
2022-09-15 14:08:48 +0200

    framework scratch with sutbs

---
+++ Genome.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class Genome:
+    def __init__(self, _value):
+        self.value = _value
+        self.fittness = 0.0
+
+
+    def _a_function(self):
+        pass
--- specialist_solution_v2.py
+++ specialist_solution_v2.py
@@ -1,8 +1,8 @@
 import numpy as np
 import random
 from numpy.random import default_rng
+from Genome import Genome
 
-mut_rate = 0.2
 
 def init_population(pop_size, _n_hidden):
     # each offspring has a list of weights with size sum_i(size(l_i-1) * size(l_i))
@@ -19,12 +19,15 @@ def init_population(pop_size, _n_hidden):
     # TODO do this more efficient!
     pop = []
     for i in range(pop_size):
-        pop.append(default_rng(seed).random(sum) + init_bias)
+        g = Genome(default_rng(seed).random(sum) + init_bias)
+        pop.append(g)
         seed += 1
 
     return np.array(pop)
 
 def mutate(pop):
+    mut_rate = 0.2
+
     pop_offspring = []
     lower = np.min(pop)
     upper = np.max(pop)
@@ -41,7 +44,29 @@ def mutate(pop):
 
     # draw probability from distribution, if prob <= mutation threshold, mutate gene
     # mutate gene by changing it into random value between lower and upper domain
-test_pop = init_population(2, np.array([2, 2]))
-print(test_pop)
-print(len(test_pop[0]))
-print(mutate(test_pop))
+
+def evaluate_fitness(pop):
+    pass
+
+def selection(pop):
+    # TODO return subset of pop
+    return np.array([])
+
+def crossover(parents_list : np.array()):
+    # TODO return list of the new offspring
+    return np.array([])
+
+if __name__=="__main__":
+    pop_size = 20
+    generations = 10
+    n_hidden = np.array([8])
+
+    pop = init_population(pop_size=pop_size, _n_hidden=n_hidden)
+    # TODO evaluation
+    for i in range(generations):
+        evaluate_fitness(pop)
+        selected_parents = selection(pop)
+        offspring = crossover(selected_parents)
+        mutate(offspring)
+
+
